66f1c94e07e5b89c22271e8ae6d5a3135a1c5518
hungbie <a0112086@u.nus.edu>
2017-02-10 17:05:19 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
+
 Route::get('/','StudentController@index');
 Route::get('student/{id}', 'StudentController@detail');
-Route::get('help', function() { return view('help');} );
+Route::get('help', 'StudentController@help');
+Route::get('login', 'StudentController@login');
+Route::post('login', 'StudentController@check');
+Route::get('logout', 'StudentController@logout');
