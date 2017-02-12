844beb4f889bfbdbad87fd76feb1059124f820d7
garbanzos <yiyant@gmail.com>
2017-02-12 21:59:21 +0800

    added ks to sum

--- public/js/sumFieldsForEditForm.js
+++ public/js/sumFieldsForEditForm.js
@@ -2,15 +2,18 @@ var mc = $('#mc');
 var tc = $('#tc');
 var hw = $('#hw');
 var bs = $('#bs');
+var ks = $('#ks');
 var ac = $('#ac');
+
 
 mc.on('keyup', recomputeSum);
 tc.on('keyup', recomputeSum);
 hw.on('keyup', recomputeSum);
 bs.on('keyup', recomputeSum);
+ks.on('keyup', recomputeSum);
 ac.on('keyup', recomputeSum);
 
-recomputeSum()
+recomputeSum();
 
 function recomputeSum() {
     $(document).ready(function () {
@@ -18,6 +21,7 @@ function recomputeSum() {
             + parseFloat(tc.val())
             + parseFloat(hw.val())
             + parseFloat(bs.val())
+            + parseFloat(ks.val())
             + parseFloat(ac.val());
 
         $('#sum').val(sum);
