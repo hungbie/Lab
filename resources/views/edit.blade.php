fe82b57e26ba3f8bc6fe3f4f7317e01328d867fa
hungbie <a0112086@u.nus.edu>
2017-02-12 15:43:36 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -29,7 +29,7 @@
   </div>
   <div class="form-group">
     {!! Form::label('mc', 'Mini contest scores:', ['class' => 'control-label']) !!}
-    {!! Form::text('mc', $data['mini_contests], ['class' => 'form-control']) !!}
+    {!! Form::text('mc', $data['mini_contests'], ['class' => 'form-control']) !!}
   </div>
   <div class="form-group">
     {!! Form::label('tc', 'Team contest scores:', ['class' => 'control-label']) !!}
